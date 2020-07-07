0eeef954713487ec3743ef51dc1b7eaa3b934ba6
Greg Martin <gregm31@live.com>
2020-07-07 15:40:44 +1000

    Added JSON Web Token to user creation

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require("express")
 const router = express.Router()
 const gravatar = require("gravatar")
 const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
+const config = require("config")
 const { check, validationResult } = require("express-validator")
 
 const User = require("../../models/User")
@@ -29,12 +31,14 @@ router.post(
 
     try {
       let user = await User.findOne({ email })
+
       // See if user exists
       if (user) {
         return res
           .status(400)
           .json({ errors: [{ msg: "User already exists" }] })
       }
+
       // Get users gravatar
       const avatar = gravatar.url(email, {
         s: "200",
@@ -48,14 +52,28 @@ router.post(
         avatar,
         password,
       })
+
       // Encrypt password
       const salt = await bcrypt.genSalt(10)
       user.password = await bcrypt.hash(password, salt)
 
       await user.save()
 
-      // Return jsonwebtoken
-      res.send("User registered")
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      }
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 36000 },
+        (err, token) => {
+          if (err) throw err
+          res.json({ token })
+        }
+      )
     } catch (err) {
       console.error(err.message)
       res.status(500).send("Server error")
