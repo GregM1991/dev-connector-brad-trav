6084c674a74787333f80e8c9f04da079aa55977e
Greg Martin <gregm31@live.com>
2020-07-11 20:32:53 +1000

    Added Delete and Get post by id

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -38,4 +38,66 @@ router.post(
     }
   }
 )
+
+// @route   GET api/posts
+// @desc    Get all posts
+// @access  Private
+router.get("/", auth, async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ date: -1 })
+    res.json(posts)
+  } catch (err) {
+    console.error(err.message)
+    res.status(500).send("Server Error")
+  }
+})
+
+// @route   GET api/posts/:id
+// @desc    Get post by id
+// @access  Private
+router.get("/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id)
+
+    if (!post) {
+      return res.status(404).json({ msg: "Post not found" })
+    }
+
+    res.json(post)
+  } catch (err) {
+    console.error(err.message)
+    if (!mongoose.Types.ObjectId.isValid(post_id)) {
+      return res.status(404).json({ msg: "Post not found" })
+    }
+    res.status(500).send("Server Error")
+  }
+})
+
+// @route   DELETE api/posts/:id
+// @desc    Delete a post
+// @access  Private
+router.delete("/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id)
+
+    if (!post) {
+      return res.status(404).json({ msg: "Post not found" })
+    }
+
+    // Check on the user
+    if (post.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: "User not authorised" })
+    }
+
+    await post.remove()
+
+    res.json({ msg: "post removed" })
+
+    res.json(posts)
+  } catch (err) {
+    console.error(err.message)
+    res.status(500).send("Server Error")
+  }
+})
+
 module.exports = router
